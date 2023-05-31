b2895cfbf8ba81cb42865905b89589ceb4d53c13
elisamunoz <elisamunozes@gmail.com>
2023-05-31 11:30:37 +0200

    created progress bar component and style it

--- src/ui/components/ProgressBar/ProgressBar.styles.tsx
+++ src/ui/components/ProgressBar/ProgressBar.styles.tsx
@@ -1,36 +1,29 @@
 import styled from 'styled-components';
 
 interface StatType {
-  value?: number;
+  value?: number | string;
 }
 
 export const StatBarWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  width: 450px;
+  align-items: center;
+  width: 100%;
   margin: 0 0.3em;
 `;
 
 export const StatBar = styled.progress<StatType>`
-  width: 100%;
-  height: 100%;
-  margin: 0.5em 0;
+  flex-grow: 1;
+  height: 0.5em;
   appearance: none;
 
   &::-webkit-progress-bar {
     border-radius: 10px;
-    background-color: #f2f2f2;
+    background: #f2f2f2;
+    background: color-mix(in oklab, currentColor, #f2f2f2);
   }
 
   &::-webkit-progress-value {
-    background-color: #4caf50;
+    background: currentColor;
     border-radius: 10px;
   }
-
-  // dolar{({ value = '' }) => {
-
-  //   if(value) {
-  //     return comilla rara background: red;comilla;
-  //   }
-  // }}
 `;
