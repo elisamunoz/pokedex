bfaab9d6c5680dfac9c800e176a6e45153f272a7
elisamunoz <elisamunozes@gmail.com>
2023-06-03 23:07:41 +0200

    404 page

---
+++ src/styles/404.styles.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const PageStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  color: #571a17;
+  font-family: -apple-system, Roboto, sans-serif, serif;
+  background: #DC0A2D;
+  text-align: center;
+`
+
+export const MainText = styled.div`
+  position: relative;
+`
+
+export const HeadingStyles = styled.h1`
+  margin: 0;
+  max-width: 320;
+  font-size: 200px;
+`
+
+export const ParagraphStyles = styled.p`
+  margin: 20px 0;
+  font-size: 35px;
+`
+
+export const Image = styled.img`
+  position: absolute;
+  max-width: -webkit-fill-available;
+  bottom: -33px;
+  right: 50%;
+  transform: translate(50%);
+`
