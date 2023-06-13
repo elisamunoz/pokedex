16cd9367474d8c23f26628479aceec7f1c63b87b
elisamunoz <elisamunozes@gmail.com>
2023-06-13 11:47:00 +0200

    fixed meta bug

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { HeadFC } from "gatsby"
-import { Helmet } from 'react-helmet';
 import Button from "../ui/components/Button"
 import { PageStyles, MainText, HeadingStyles, ParagraphStyles, ImageContainer, StyledLink } from "../styles/404.styles";
 import rocketTeamImg from "../images/rocketTeam.png" 
@@ -13,23 +12,18 @@ const NotFoundPage: React.FC<Props> = ({
   image = rocketTeamImg
 }) => {
   return (
-    <>
-      <Helmet>
-        <title>404</title>
-      </Helmet>
-      <PageStyles>
-      <MainText>
-      <ImageContainer src={image} alt="Rocket Team Image" />
-      <HeadingStyles>404</HeadingStyles>
-      </MainText>
-      <ParagraphStyles>
-        Page Not Found
-      </ParagraphStyles>
-      <Button isVisible={true} name="Go to main page">
-        <StyledLink to="/">Go to main page</StyledLink>
-      </Button>
-    </PageStyles>
-    </>
+    <PageStyles>
+    <MainText>
+    <ImageContainer src={image} alt="Rocket Team Image" />
+    <HeadingStyles>404</HeadingStyles>
+    </MainText>
+    <ParagraphStyles>
+      Page Not Found
+    </ParagraphStyles>
+    <Button isVisible={true} name="Go to main page">
+      <StyledLink to="/">Go to main page</StyledLink>
+    </Button>
+  </PageStyles>
   )
 }
 
